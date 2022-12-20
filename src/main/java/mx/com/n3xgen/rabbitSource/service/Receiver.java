8f56a3e69e9355591f48e4805d3519102a47565f
EmmanuelArreola <emmanuel.arreola@cloudgensys.com>
2022-12-20 11:05:35 -0600

    Before changing to another API

---
+++ src/main/java/mx/com/n3xgen/rabbitSource/service/Receiver.java
@@ -0,0 +1,16 @@
+package mx.com.n3xgen.rabbitSource.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+	private static final Logger logger = LoggerFactory.getLogger(Receiver.class);
+
+	public void receiveMessage(String message) {
+		logger.debug("Received <" + message + ">");
+
+	}
+}
--- src/main/java/mx/com/n3xgen/rabbitSource/stream/ReceiveMessageConfiguration.java
+++ src/main/java/mx/com/n3xgen/rabbitSource/stream/ReceiveMessageConfiguration.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Supplier;
 
+import org.springframework.amqp.support.ConsumerTagStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -21,29 +22,33 @@ import com.rabbitmq.client.DeliverCallback;
 
 import lombok.extern.log4j.Log4j2;
 import mx.com.n3xgen.rabbitSource.bean.RabbitMQProperties;
+import mx.com.n3xgen.rabbitSource.service.Receiver;
 import reactor.core.publisher.Flux;
 
 @Log4j2
 @EnableConfigurationProperties({ RabbitMQProperties.class })
 @Configuration
 public class ReceiveMessageConfiguration {
-	private final static String QUEUE_NAME = "UniversalId";
+	private final static String QUEUE_NAME = "UniversalTest";
 	String payload;
 	Connection connection;
 	Channel channel;
-
-//	public ReceiveMessageConfiguration() throws Exception{
-//		super();
-//		receiverMessage(null).get();
-//	}
+	long deliveryTag;
 
 	@Bean
 	public Supplier<String> receiverMessage(RabbitMQProperties rabbitMQProperties) throws Exception {
 		return () -> {
 			try {
+//
+//				payload = "";
+//				connection = null;
+//				channel = null;
+//				deliveryTag = 0;
+
 				ConnectionFactory factory = new ConnectionFactory();
 				factory.setHost("localhost");
 				factory.setRequestedHeartbeat(0);
+				factory.setShutdownExecutor(null);
 
 				connection = factory.newConnection();
 				channel = connection.createChannel();
@@ -53,6 +58,7 @@ public class ReceiveMessageConfiguration {
 
 				DeliverCallback deliverCallback = (consumerTag, delivery) -> {
 					String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+					deliveryTag = delivery.getEnvelope().getDeliveryTag();
 //	          	Get headers from properties
 					AMQP.BasicProperties properties = delivery.getProperties();
 					Map<String, Object> headers = properties.getHeaders();
@@ -69,6 +75,8 @@ public class ReceiveMessageConfiguration {
 				};
 				channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
 				});
+//				channel.basicAck(deliveryTag, false);
+//				channel.basicCancel(consumerTag);
 				log.info(payload);
 				if (payload != null) {
 					return payload;
@@ -86,7 +94,7 @@ public class ReceiveMessageConfiguration {
 				}
 			}
 			return new String("No message");
-
 		};
 	}
+
 }
